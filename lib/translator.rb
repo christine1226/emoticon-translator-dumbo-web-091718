cd7f0d367a67312377134698dcf4b690a7338330
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 21:50:15 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 require 'yaml'
 
-def load_library(YAML.load_file('emoticons.yml'))
+def load_library(path)
   emoticons = YAML.load_file('emoticons.yml')
   emoticon_hash = {}
   emoticon_hash["get_emoticon"] = {}
